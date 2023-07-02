f06c9f0c6da86786f1571e7e7c5ee7300ea500b3
pangpangstudy <zhangzh0214@outlook.com>
2023-07-02 16:27:54 +0800

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,33 @@
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category, Filter, NFTCard, Collection, FollowerTab, AudioLive, Slider, Brand, Video } from "../components/componentsindex";
+import { TiThList } from "react-icons/ti";
+ 
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSlider />
+      <Title heading="Latest Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+       />
+      <AudioLive />
+      <FollowerTab />
+      <Slider />
+      <Collection />
+      <Title heading="Featured NFTs"
+      paragraph="Discover the most outstanding NFTs in all topics of life." />
+      <Filter />
+      <NFTCard />
+      <Title heading="Browse by Category" paragraph="Explore the NFTs in the most featured categories." />
+      <Category />
+      <Subscribe />
+      <Brand />
+      <Video />
+    </div>
+  );
+};
+
+export default Home;
